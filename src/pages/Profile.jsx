1bf7c2096bd47dcaabf798f39698190bdc986e85
Ahmet Furkan Sayan <ahmet.sayan331@gmail.com>
2024-09-12 00:21:12 +0300

    some problems were fixed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,41 +4,42 @@ import { Formik, Form, ErrorMessage, useFormik } from 'formik';
 import { Box, Button, CircularProgress, TextField, Typography, Alert, FormHelperText, Avatar } from '@mui/material';
 import { useNavigate } from 'react-router-dom'
 import { notify } from '../utils/toastMessage';
-import { loginSchema } from '../validations/loginSchema';
 import { useDispatch, useSelector } from 'react-redux';
 import { profileSchema } from '../validations/profileSchema';
-import { getUserData } from '../redux/slices/userSlice';
-let i = 0;
+import { getUserData, selectUserData } from '../redux/slices/userSlice';
+import { setIsLoading } from '../redux/slices/appSlice';
+
+// import { selectUserData, } from '../redux/slices/userSlice';
 
 function Profile() {
-    const [avatar, setAvatar] = useState(null);
-    const isLoading = useSelector((state) => state.app.isLoading)
-    const fileInputRef = useRef(null);
     const dispatch = useDispatch();
+    const [avatar, setAvatar] = useState(null);
+    const isLoading = useSelector(({ app }) => app.isLoading)
+    const fileInputRef = useRef(null);
 
     const handleAvatarClick = () => {
         if (fileInputRef.current) {
             fileInputRef.current.click();
         }
     };
-    // const [formVals, setFormVals] = useState({ email: '', newPassword: '', username: '' });
+
+    const userData = useSelector(selectUserData)
     const userToken = useSelector((state) => state.auth.userToken)
-    const userData = useSelector(({ user }) => {
-        console.log('calisti ' + i + user);
-        i++;
-        return {
-            email: user?.email || '',
-            emailVerified: user?.emailVerified || false,
-            localId: user?.localId || '',
-        }
-    });
+
 
     useEffect(() => {
-        dispatch(getUserData(userToken));
+        const fetchUserData = async (token) => {
+            dispatch(setIsLoading(true));
+            try {
+                dispatch(getUserData(token))
+            } catch (error) {
+                console.log(error);
+            } finally {
+                dispatch(setIsLoading(false));
+            }
+        };
+        fetchUserData(userToken);
     }, [dispatch, userToken]);
-
-
-
 
 
     const handleFileChange = (event) => {
@@ -79,98 +80,106 @@ function Profile() {
                 </Grid>
 
                 <Grid size={12} mt={0} mb={0} bgcolor={'#303030'} p={5} display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'} >
-                    <Formik
-                        initialValues={formik.initialValues}
-                        onSubmit={formik.onSubmit}
-                        enableReinitialize
-                        validationSchema={profileSchema}
-                    >
+                    {isLoading ? (<CircularProgress color="primary" />
+                    ) : (
+                        <Formik
+                            initialValues={{
+                                email: userData.email || '',
+                                username: '',
+                                newPassword: '',
+                            }}
+                            onSubmit={formik.onSubmit}
+                            enableReinitialize
+                            validationSchema={profileSchema}
+                        >
 
-                        {({ handleChange, handleBlur, values, errors, touched }) => (
-                            <Form>
-                                <Box justifyContent={'center'} alignItems={'center'} display={'flex'}
-                                    sx={{ width: '10em', height: '10em' }}
-                                    mb={2}>
-                                    <Avatar
-                                        variant='circular'
-                                        sx={{
-                                            cursor: 'pointer', width: '100%', height: '100%',
-                                        }}
-                                        onClick={handleAvatarClick}
-                                        src={avatar}
-                                    />
-                                    <input
-                                        type="file"
-                                        ref={fileInputRef}
-                                        style={{ display: 'none' }}
-                                        onChange={handleFileChange}
-                                    />
-                                </Box>
+                            {({ handleChange, handleBlur, values, errors, touched, }) => (
+                                <Form >
+                                    <Box justifyContent={'center'} alignItems={'center'} display={'flex'}
+                                        sx={{ width: '10em', height: '10em' }}
+                                        mb={2}>
+                                        <Avatar
+                                            variant='circular'
+                                            sx={{
+                                                cursor: 'pointer', width: '100%', height: '100%',
+                                            }}
+                                            onClick={handleAvatarClick}
+                                            src={avatar}
+                                        />
+                                        <input
+                                            type="file"
+                                            ref={fileInputRef}
+                                            style={{ display: 'none' }}
+                                            onChange={handleFileChange}
 
-                                <Box display={'flex'} mb={2}>
-                                    <TextField
-                                        type='email'
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        slotProps={{
-                                            input: {
-                                                readOnly: true,
+                                        />
+                                    </Box>
 
-                                            }
-                                        }}
-                                        value={values.email}
+                                    <Box display={'flex'} mb={2}>
+                                        <TextField
+                                            type='email'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            slotProps={{
+                                                input: {
+                                                    readOnly: true,
+                                                }
+                                            }}
+                                            value={values.email}
+                                            fullWidth
+                                            variant='standard'
+                                            label='E-mail'
+                                            name="email"
+                                            id="email"
+                                        // error={touched.email && Boolean(errors.email)}
+                                        // helperText={touched.email && errors.email}
+                                        />
+                                    </Box>
+                                    <Box display={'flex'} mb={2}>
+                                        <TextField
+                                            type='text'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            fullWidth
+                                            variant='standard'
+                                            value={values.username}
+                                            label='Kullanıcı Adı'
+                                            name="username"
+                                            id="username"
+                                            error={touched.username && Boolean(errors.username)}
+                                            helperText={touched.username && errors.username}
+                                        />
+                                    </Box>
+                                    <Box display={'flex'} mb={2}>
+                                        <TextField
+                                            type='password'
+                                            onChange={handleChange}
+                                            value={values.newPassword}
+                                            onBlur={handleBlur}
+                                            autoComplete='off'
+                                            fullWidth
+                                            variant='standard'
+                                            label='Yeni Şifre'
+                                            name="newPassword"
+                                            id='newPpassword'
+                                            error={touched.password && Boolean(errors.password)}
+                                            helperText={touched.password && errors.password}
+                                        />
+                                    </Box>
+                                    <Box display={'flex'} mt={3}>
+                                        <Button fullWidth
+                                            type='submit'
+                                            disabled={isLoading}
+                                            variant='outlined'>
+                                            {(isLoading) ? <CircularProgress color='primary' size={'2em'} /> : 'Kaydet'}
+                                        </Button>
+                                    </Box>
+                                </Form>
+                            )}
+                        </Formik>
 
-                                        fullWidth
-                                        variant='standard'
-                                        label='E-mail'
-                                        name="email"
-                                        id="email"
-                                    // error={touched.email && Boolean(errors.email)}
-                                    // helperText={touched.email && errors.email}
-                                    />
-                                </Box>
-                                <Box display={'flex'} mb={2}>
-                                    <TextField
-                                        type='text'
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        fullWidth
-                                        variant='standard'
-                                        value={values.username}
-                                        label='Kullanıcı Adı'
-                                        name="username"
-                                        id="username"
-                                    // error={touched.email && Boolean(errors.email)}
-                                    // helperText={touched.email && errors.email}
-                                    />
-                                </Box>
-                                <Box display={'flex'} mb={2}>
-                                    <TextField
-                                        type='password'
-                                        onChange={handleChange}
-                                        value={values.password}
-                                        onBlur={handleBlur}
-                                        autoComplete='off'
-                                        fullWidth
-                                        variant='standard'
-                                        label='Yeni Şifre'
-                                        name="newPassword"
-                                        id='newPpassword'
-                                        error={touched.password && Boolean(errors.password)}
-                                        helperText={touched.password && errors.password}
-                                    />
-                                </Box>
-                                <Box display={'flex'} mt={3}>
-                                    <Button fullWidth
-                                        type='submit'
-                                        disabled={isLoading}
-                                        variant='outlined'>
-                                        {(isLoading) ? <CircularProgress color='primary' size={'2em'} /> : 'Kaydet'}
-                                    </Button>
-                                </Box>
-                            </Form>
-                        )}
-                    </Formik>
+                    )}
+
 
                 </Grid>
 
--- src/redux/slices/userSlice.jsx
+++ src/redux/slices/userSlice.jsx
@@ -1,17 +1,19 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { getUserDetails } from '../../services/userService';
+import { createSelector } from 'reselect';
 
 const userInitialState = {
-
     localId: null,
     email: null,
     emailVerified: false,
+    status: 'idle',  // 'idle', 'loading', 'succeeded', 'failed' gibi durumlar
+    error: null,
 };
 
 export const getUserData = createAsyncThunk('user/getUserData', async (userToken) => {
     try {
         const response = await getUserDetails(userToken);
-        return response;
+        return response;  // Payload olarak döner
     } catch (error) {
         console.error('Error fetching user data:', error);
         throw new Error('Unable to fetch user data');
@@ -19,31 +21,38 @@ export const getUserData = createAsyncThunk('user/getUserData', async (userToken
 });
 
 export const userSlice = createSlice({
-
     name: 'user',
     initialState: userInitialState,
-    reducers: {
-
-    },
-
+    reducers: {},
     extraReducers: (builder) => {
         builder
-            .addCase(getUserData.pending, (state, action) => {
-                // state.userData = action.payload;
+            .addCase(getUserData.pending, (state) => {
+                state.status = 'loading';  // Yüklenme durumu
+                state.error = null;  // Önceki hataları sıfırla
             })
             .addCase(getUserData.fulfilled, (state, action) => {
-                // console.log(action.payload)
+                state.status = 'succeeded';
                 state.email = action.payload.email;
                 state.localId = action.payload.localId;
                 state.emailVerified = action.payload.emailVerified;
-
             })
             .addCase(getUserData.rejected, (state, action) => {
-                // state.userData = action.payload;
-            })
-
-    }
+                state.status = 'failed';  // Yüklenme başarısız
+                state.error = action.error.message;  // Hata mesajını kaydet
+            });
+    },
 });
 
+
+const selectUserState = (state) => state.user;
+
+export const selectUserData = createSelector(
+    [selectUserState],
+    (user) => ({
+        email: user.email,
+        emailVerified: user.emailVerified,
+        localId: user.localId
+    })
+);
 
 export default userSlice.reducer;
